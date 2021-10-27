dd58d3759ad6d8d6512e2232156c26315b3919c5
juwitlistyasari <80829879+juwitlistyasari@users.noreply.github.com>
2021-10-27 19:20:35 +0700

    Add files via upload

---
+++ konstantaa.php
@@ -0,0 +1,9 @@
+<?php
+define ("hobi", "Dengerin lagu nostalgia");
+echo "Hei guys, hobbi saya :";
+echo hobi;
+echo "<br>";
+$jl=14;
+echo "dan saya tinggal di Jl. Cempaka No.  Kota Probolinggo ";
+echo $jl;
+?>
